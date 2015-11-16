0dff6f924d0ef1523af00fce019f4669c49d147a
Renaat De Muynck <renaat.demuynck@gmail.com>
2015-11-16 11:07:42 +0100

    Add unit tests for indeterminable.js

--- tests/indeterminable-test.js
+++ tests/indeterminable-test.js
@@ -7,4 +7,26 @@ $(function () {
     
     QUnit.module('indeterminable.js');
     
+    QUnit.test('Regular checkbox cannot be set to "indeterminate"', function(assert) {
+        var checkbox = $('<input type="checkbox">');
+        
+        fixture.append(checkbox);
+        
+        S(checkbox).click(function () {
+            assert.equal(checkbox.prop('indeterminate'), false, 'Radio button should be checked');
+        });
+    });
+    
+    QUnit.test('Left click sets unckecked checkbox to "indeterminate"', function(assert) {
+        var checkbox = $('<input type="checkbox">');
+        
+        fixture.append(checkbox);
+        
+        checkbox.prop('indeterminable', true);
+        
+        S(checkbox).click(function () {
+            assert.equal(checkbox.prop('indeterminate'), true, 'Radio button should be checked');
+        });
+    });
+    
 });
