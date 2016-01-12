5b438df21def64a22249e2632d35d86785944159
Renaat De Muynck <renaat.demuynck@gmail.com>
2016-01-12 14:40:14 +0100

    Add doc comments in source

--- src/indeterminable.js
+++ src/indeterminable.js
@@ -3,6 +3,12 @@
     
     'use strict';
     
+    /**
+     * Finds the given label's corresponding checkbox
+     * 
+     * @param {(HTMLLabelElement|HTMLInputElement)} A label or input element
+     * @returns {HTMLInputElement} The corresponding checkbox or NULL if not found
+     */
     function findCheckbox(el) {
         switch (el.tagName) {
             case 'INPUT':
@@ -14,20 +20,36 @@
         }
     }
     
+    // Listen for the mouseup event
+    // This is only time we can know the current checked state of the checkbox
+    // AND that we can be sure the click event will be fired
     document.addEventListener('mouseup', function (e) {
         var checkbox = findCheckbox(e.target);
         
+        /**
+         * Handler that will set the checkbox to the indeterminate state and unregister itself
+         * 
+         * @param {MouseEvent} e The mouse event object
+         */
         function onclick(e) {
             checkbox.indeterminate = true;
             checkbox.checked = false;
             checkbox.removeEventListener('click', onclick, false);
         }
         
+        /**
+         * Handler that will set the checkbox to the checked state and unregister itself
+         * 
+         * @param {MouseEvent} e The mouse event object
+         */
         function onclickIndeterminate(e) {
             checkbox.checked = true;
             checkbox.removeEventListener('click', onclickIndeterminate, false);
         }
         
+        
+        // Attach the handler if the radio button's 'uncheckable' property is TRUE
+        // when the checkbox is clicked with the left mouse button
         if (checkbox && e.which === 1) {
             // Fix for IE's non-standard behaviour when clicking an indeterminate checkbox
             if (checkbox.indeterminate) {
@@ -36,8 +58,10 @@
                 checkbox.addEventListener('click', onclick, false);
             }
         }
+        
     }, true);
     
+    // Define the 'indeterminable' property on the input element prototype
     Object.defineProperty(HTMLInputElement.prototype, 'indeterminable', {
         value: false,
         writable: true
--- src/uncheckable.js
+++ src/uncheckable.js
@@ -3,6 +3,12 @@
     
     'use strict';
     
+    /**
+     * Finds the given label's corresponding radio button
+     * 
+     * @param {(HTMLLabelElement|HTMLInputElement)} A label or input element
+     * @returns {HTMLInputElement} The corresponding radio button or NULL if not found
+     */
     function findRadioButton(el) {
         switch (el.tagName) {
             case 'INPUT':
@@ -14,19 +20,31 @@
         }
     }
     
+    // Listen for the mouseup event
+    // This is only time we can know the current checked state of the radio button
+    // AND that we can be sure the click event will be fired
     document.addEventListener('mouseup', function (e) {
         var radio = findRadioButton(e.target);
         
+        /**
+         * Handler that will uncheck the radio button and unregister itself
+         * 
+         * @param {MouseEvent} e The mouse event object
+         */
         function onclick(e) {
             radio.checked = false;
             radio.removeEventListener('click', onclick, false);
         }
         
+        // Attach the handler if the radio button's 'uncheckable' property is TRUE
+        // and is in a checked state but only when clicked with the left mouse button
         if (radio && radio.uncheckable && radio.checked && e.which === 1) {
             radio.addEventListener('click', onclick, false);
         }
+        
     }, true);
     
+    // Define the 'uncheckable' property on the input element prototype
     Object.defineProperty(HTMLInputElement.prototype, 'uncheckable', {
         value: false,
         writable: true
